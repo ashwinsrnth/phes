2fbe6a02db5112f3172a46adc379895e5a39ae00
Ashwin Srinath <ashwinsrnth@gmail.com>
2014-10-03 11:33:01 -0400

    added z-diffusion test

--- test_diffusion_kernel.py
+++ test_diffusion_kernel.py
@@ -50,4 +50,28 @@ class TestDiffusionKernel(object):
         f2 = f2_gpu.get()
         assert(f2[1,1,1] != 2.0)
 
+    
+    def test_z_diffusion(self):
 
+        # test diffusion in the z-direction:
+        
+        f = np.ones([3, 3, 3], dtype=np.float64)
+        f = f*np.array([1, 2, 3], dtype=np.float64)
+        f = np.transpose(f, [2, 1, 0])
+        f_gpu = gpuarray.to_gpu(f)
+
+        f2 = np.zeros([3, 3, 3], dtype=np.float64)
+        f2_gpu = gpuarray.to_gpu(f2)
+        
+        # run the kernel:
+        self.func.prepare([np.intp, np.intp, np.float64, np.float64,
+                             np.intc, np.intc, np.intc, 
+                                 np.float64, np.float64, np.float64])
+
+        self.func.prepared_call((1,1,1), (16, 16, 1), 
+                           f_gpu.gpudata, f2_gpu.gpudata, 1., 1., 3, 3, 3, 1, 1, 1)
+
+        print f_gpu
+        print f2_gpu
+        f2 = f2_gpu.get()
+        assert(f2[1,1,1] == 2.0)
