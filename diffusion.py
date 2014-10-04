3e7cf41d626a113086764b4aa77702b5595c413b
Ashwin Srinath <ashwinsrnth@gmail.com>
2014-10-04 11:18:28 -0400

    minor changes

--- diffusion.py
+++ diffusion.py
@@ -24,9 +24,9 @@ mod = cuda.module_from_file('diffusion_kernel.cubin')
 func = mod.get_function('temperature_update16x16')
 
 # local sizes:
-nx = 398
-ny = 398
-nz = 398
+nx = 126
+ny = 126
+nz = 128
 
 # global lengths:
 lx = 1.0
@@ -34,8 +34,8 @@ ly = 1.0
 lz = 1.0
 
 # material properties:
-alpha = 1e-5
-dt = 0.01
+alpha = 1e-4
+dt = 0.0001
 
 dx = lx/((npx*nx)-1)
 dy = ly/((npy*ny)-1)
@@ -60,7 +60,7 @@ T1_local = np.zeros([nz+2, ny+2, nx+2], dtype=np.float64)
 T1_global = np.zeros([nz, ny, nx], dtype=np.float64)
 
 # set initial conditions:
-set_boundary_values(comm, T1_local, (0., 0., 0., 0., 0., 500.))
+set_boundary_values(comm, T1_local, (100., 0., 0., 100., 0., 500.))
 
 # transfer to gpu:
 T2_local_gpu = gpuarray.to_gpu(T2_local)
@@ -76,6 +76,7 @@ comp_time = np.zeros(1000)
 
 
 t_start = MPI.Wtime()
+
 for step in range(1000):
 
     t1 = MPI.Wtime()
@@ -88,17 +89,17 @@ for step in range(1000):
                         nx+2, ny+2, nz+2,
                         dx, dy, dz)
 
-    da.local_to_global(T2_local_gpu, T1_global_gpu)
     t3 = MPI.Wtime()
+    da.local_to_global(T2_local_gpu, T1_global_gpu)
 
     comp_time[step] = t3-t2
     comm_time[step] = t2-t1
 
 t_end = MPI.Wtime()
 
-if rank == 0:
-    print 'Computation: ', comp_time.mean()*1000
-    print 'Communication: ', comm_time.mean()*1000
+if rank == 2:
+    print 'Computation: ', comp_time.sum()
+    print 'Communication: ', comm_time.sum()
     print 'Total: ', t_end-t_start
 
 MPI.Finalize()
