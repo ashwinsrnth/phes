3a4c8d1fd575bc39be808b3636c28c95da522f41
Ashwin Srinath <ashwinsrnth@gmail.com>
2014-10-04 14:42:06 -0400

    Kernel is timed using CUDA events

--- diffusion.py
+++ diffusion.py
@@ -4,6 +4,7 @@ from pycuda import autoinit
 import pycuda.gpuarray as gpuarray
 import numpy as np
 
+import time
 import sys
 sys.path.append('gpuDA')
 
@@ -24,18 +25,18 @@ mod = cuda.module_from_file('diffusion_kernel.cubin')
 func = mod.get_function('temperature_update16x16')
 
 # local sizes:
-nx = 126
-ny = 126
-nz = 128
+nx = 254
+ny = 254
+nz = 256
 
 # global lengths:
-lx = 1.0
-ly = 1.0
-lz = 1.0
+lx = 0.3
+ly = 0.3
+lz = 0.3
 
 # material properties:
-alpha = 1e-4
-dt = 0.0001
+alpha = 1e-6
+dt = 0.01
 
 dx = lx/((npx*nx)-1)
 dy = ly/((npy*ny)-1)
@@ -60,7 +61,7 @@ T1_local = np.zeros([nz+2, ny+2, nx+2], dtype=np.float64)
 T1_global = np.zeros([nz, ny, nx], dtype=np.float64)
 
 # set initial conditions:
-set_boundary_values(comm, T1_local, (100., 0., 0., 100., 0., 500.))
+set_boundary_values(comm, T1_local, (500., 0., 0., 100., 0., 500.))
 
 # transfer to gpu:
 T2_local_gpu = gpuarray.to_gpu(T2_local)
@@ -69,35 +70,39 @@ T1_global_gpu = gpuarray.to_gpu(T1_global)
 
 da.local_to_global(T1_local_gpu, T1_global_gpu)
 
-np.set_printoptions(precision=2)
+#np.set_printoptions(precision=2)
 
-comm_time = np.zeros(1000)
-comp_time = np.zeros(1000)
+nsteps = 100
+comm_time = np.zeros(nsteps)
+comp_time = np.zeros(nsteps)
 
+t_start = time.time()
 
-t_start = MPI.Wtime()
+start = cuda.Event()
+end = cuda.Event()
 
-for step in range(1000):
+for step in range(nsteps):
 
-    t1 = MPI.Wtime()
+    t1 = time.time()
     da.global_to_local(T1_global_gpu, T1_local_gpu)
-    t2 = MPI.Wtime()
+    t2 = time.time()
 
+    start.record()
     func.prepared_call(((nx+2)/16, (ny+2)/16, 1), (16, 16, 1),
                         T1_local_gpu.gpudata, T2_local_gpu.gpudata,
                         alpha, dt,
                         nx+2, ny+2, nz+2,
                         dx, dy, dz)
-
-    t3 = MPI.Wtime()
+    end.record()
+    end.synchronize()
+    comp_time[step] = start.time_till(end) * 1e-3
+    
     da.local_to_global(T2_local_gpu, T1_global_gpu)
-
-    comp_time[step] = t3-t2
     comm_time[step] = t2-t1
+    
+t_end = time.time()
 
-t_end = MPI.Wtime()
-
-if rank == 2:
+if rank == 0:
     print 'Computation: ', comp_time.sum()
     print 'Communication: ', comm_time.sum()
     print 'Total: ', t_end-t_start
