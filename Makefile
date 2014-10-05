804d1407c7b924255fa609481041bc7a11320de0
Ashwin Srinath <ashwinsrnth@gmail.com>
2014-10-05 19:18:10 -0400

    added nose tests for set_boundary

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 CUDA_FLAGS=-arch=sm_30
-
+MPI_FLAGS=--mca pml ob1 --mca btl_openib_cuda_want_gdr 0
 
 test:
 	make test_kernel
+	make test_set_boundary
 
 kernel: diffusion_kernel.cu
 	nvcc -cubin ${CUDA_FLAGS} diffusion_kernel.cu
@@ -11,6 +12,9 @@ test_kernel: diffusion_kernel.cu test_diffusion_kernel.py
 	make kernel
 	nosetests --nocapture test_diffusion_kernel.py
 	rm *.pyc
+
+test_set_boundary: test_set_boundary.py
+	mpiexec -n 27 ${MPI_FLAGS} nosetests test_set_boundary.py
 
 clean:
 	rm -f *.pyc
--- test_set_boundary.py
+++ test_set_boundary.py
@@ -1,20 +1,25 @@
 from set_boundary import *
+import nose
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
+class TestSetBoundary:
+    
+    def setup(self):
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+        size = comm.Get_size()
 
-npx = 3
-npy = 3
-npz = 3
+        npx = 3
+        npy = 3
+        npz = 3
+        assert(size == 27)
 
-assert(npx*npy*npz == size)
+        self.comm = comm.Create_cart([npz, npx, npy])
+        self.rank = comm.Get_rank()
 
-comm = comm.Create_cart([npz, npx, npy])
-f_local = np.zeros([3,3,3],dtype=np.float64)
+    def test_set_boundary_values(self):
+        f_local = np.zeros([3,3,3],dtype=np.float64)
+        set_boundary_values(self.comm, f_local, [1, 2, 3, 4, 5, 6])
+        if self.rank == 0 or self.rank == 12 or self.rank == 24:
+            assert(f_local[1,1,0] == 1)
 
-set_boundary_values(comm, f_local, [1, 2, 3, 4, 5, 6])
-
-if rank == 0 or rank == 12 or rank == 24:
-    assert(f_local[1,1,0] == 1)
 
